86ffce0620198c6c25d68ac56852e09747917c3b
Ehsan-c137 <ehsanmousavi1.79@gmail.com>
2021-12-10 13:05:12 +0330

    added close btn to menu

--- Frontend-Mentorship-Challenge-06/assets/script.js
+++ Frontend-Mentorship-Challenge-06/assets/script.js
@@ -1,13 +1,14 @@
 "use strict";
 
 const modalHamburgerBtn = document.querySelector(".modal_hamburger");
-const modalContainer = document.querySelector("#modal-container");
+const modalHamburgerOpen = document.querySelector(".modal-hamburger-open");
+const closeHamBtn = document.querySelector(".close-ham-btn");
 const mainContainerSub = document.querySelector(".main-container-sub");
 
-const m = document.querySelector("#second-container");
-console.log(m);
 const openModal = function () {
    mainContainerSub.classList.toggle("show-menu");
+   closeHamBtn.classList.toggle("show-close-btn");
+   modalHamburgerOpen.classList.toggle("modal-hamburger-open-hidden");
 };
 
 modalHamburgerBtn.addEventListener("click", openModal);
