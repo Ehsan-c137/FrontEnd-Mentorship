f9d018132ed9f4830903a693d74cb4b18dab0115
Ehsan-c137 <ehsanmousavi1.79@gmail.com>
2021-12-17 21:39:23 +0330

    added currentDay api

--- Frontend-Mentorship-Challenge-06/assets/JS/script.js
+++ Frontend-Mentorship-Challenge-06/assets/JS/script.js
@@ -8,8 +8,6 @@ const currentTime = document.querySelector(".current-time");
 
 const openModal = function () {
    mainContainerSub.classList.toggle("show-menu");
-   closeHamBtn.classList.toggle("show-close-btn");
-   modalHamburgerOpen.classList.toggle("modal-hamburger-open-hidden");
 };
 modalHamburgerBtn.addEventListener("click", openModal);
 
@@ -26,4 +24,45 @@ const weekday = [
    "Saturday",
 ];
 let day = weekday[time.getDay()];
-document.querySelector(".current-date").textContent = day;
+document.querySelector(".current-date").textContent = `${day}, `;
+
+// GET CURRENT WEATHER DATA
+let weather = {
+   apiKey: "fdf7cba70862ec4cdb5083aebdd424b7",
+   fetchWeather: function (city) {
+      fetch(
+         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+      )
+         .then((response) => response.json())
+         .then((data) => this.displayWeather(data));
+   },
+   displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+
+      document.querySelector(".city-name").textContent = name;
+      document.querySelector(
+         ".first-container__overal-left img"
+      ).src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+      document.querySelector(
+         ".description"
+      ).textContent = `${day}, ${description}`;
+      document.querySelector(".main-temp").textContent = `${Math.trunc(temp)}°`;
+      document.querySelector(".humidity").textContent = `${humidity}%`;
+   },
+   // search: function () {
+   //    this.fetchWeather(document.querySelector(".search-bar").value);
+   // },
+};
+document
+   .querySelector(".search-input button")
+   .addEventListener("click", function () {
+      // weather.search();
+      console.log(document.querySelector(".search-bar").value);
+   });
+document
+   .querySelector(".search-bar")
+   .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") weather.search();
+   });
