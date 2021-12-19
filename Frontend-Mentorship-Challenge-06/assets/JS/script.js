e05e31be6ff3b753cb6bf554c48cdd291dc9d20e
Ehsan-c137 <ehsanmousavi1.79@gmail.com>
2021-12-20 01:48:11 +0330

    added chartJs data dynamicly

--- Frontend-Mentorship-Challenge-06/assets/JS/script.js
+++ Frontend-Mentorship-Challenge-06/assets/JS/script.js
@@ -26,6 +26,9 @@ const weekday = [
 let day = weekday[time.getDay()];
 document.querySelector(".current-date").textContent = `${day}`;
 
+function destroyChart() {
+   myChart.destroy();
+}
 // GET CURRENT WEATHER DATA
 let weather = {
    apiKey: "fdf7cba70862ec4cdb5083aebdd424b7",
@@ -70,35 +73,94 @@ let weather = {
       document.querySelector(".sunset").textContent = sunsetTime;
       document.querySelector(".sunrise").textContent = sunriseTime;
 
-      // uv index
+      // uv index & daily humidity chart
       fetch(
-         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${this.apiKey}`
+         `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&&units=metric&appid=${this.apiKey}`
       )
          .then((response) => response.json())
-         .then((data) => showUV(data));
+         .then((data) => {
+            showUV(data);
+            showDailyHumidity(data);
+         });
+
       function showUV(data) {
          const { uvi } = data.current;
          document.querySelector(".uvindex").textContent = ` ${Math.trunc(
             uvi
          )} of 10`;
       }
+
+      function showDailyHumidity(data) {
+         // get data for week
+         const humidityData = [];
+         for (let i = 0; i <= 7; i++) {
+            humidityData.push(data.daily[i].humidity);
+         }
+         // get days of week
+         const chartJsWeek = [];
+         for (let i = 0; i < 7; i++) {
+            let convertedUnixTime = new Date(
+               data.daily[i].dt * 1000
+            ).toLocaleString("en-US", {
+               weekday: "short",
+            });
+
+            chartJsWeek.push(convertedUnixTime);
+         }
+
+         const chartData = {
+            labels: chartJsWeek,
+            datasets: [
+               {
+                  label: "% humidity",
+                  data: humidityData,
+                  fill: true,
+                  tension: 0.3,
+                  responsive: true,
+                  borderWidth: 1,
+               },
+            ],
+         };
+         const chartConfig = {
+            type: "line",
+            data: chartData,
+            options: {
+               maintainAspectRatio: false,
+               scales: {
+                  y: {
+                     beginAtZero: true,
+                  },
+               },
+            },
+         };
+
+         let myChart = new Chart(
+            document.getElementById("myChart"),
+            chartConfig
+         );
+      }
       // change the background According to location
       // need vpn for iran
-      // document.querySelector("#first-container").style.backgroundImage =
-      //    "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      // document.querySelector(
+      //    "#first-container"
+      // ).style.backgroundImage = `url(https://source.unsplash.com/1600x900/?${name})`;
    },
 
    search: function () {
       this.fetchWeather(document.querySelector(".search-bar").value);
    },
 };
+
 document
    .querySelector(".search-input button")
    .addEventListener("click", function () {
       weather.search();
+      myChart.update();
    });
 document
    .querySelector(".search-bar")
    .addEventListener("keyup", function (event) {
-      if (event.key == "Enter") weather.search();
+      if (event.key == "Enter") {
+         weather.search();
+      }
    });
